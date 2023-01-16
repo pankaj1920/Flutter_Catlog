31447d6db022b5c92419b18dd69e8c697281b228
pankaj1920 <42286224+pankaj1920@users.noreply.github.com>
2023-01-16 07:12:43 +0530

    Text Style

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(child: Text("This is Login Page")),
+    return const Material(
+      child: Center(
+          child: Text(
+        "This is Login Page",
+        style: TextStyle(
+            fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold),
+      )),
     );
   }
 }
