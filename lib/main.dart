723a7dc7a27d0507ca84e0193040421ed5a6f064
pankaj1920 <42286224+pankaj1920@users.noreply.github.com>
2023-01-18 06:38:47 +0530

    Login Ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:catalog/pages/home_page.dart';
 import 'package:catalog/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily),
       routes: {
         "/": ((context) => LoginPage()),
         "/home": ((context) => HomePage()),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,13 +3,37 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-          child: Text(
-        "This is Login Page",
-        style: TextStyle(
-            fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold),
-      )),
-    );
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/img_login.png",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              "Welcome",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 38.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter username", labelText: "User Name"),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
